4ee5501b672cc2262bb5b7382cfe2e06d251bf6e
ChaoZhou <v-zhoucha@microsoft.com>
2023-03-13 14:45:41 +0000

    update client to train resnet18 on mnist

--- src/client.py
+++ src/client.py
@@ -4,9 +4,14 @@ Date: 2023.03.13
 this file contains benign client and malious client
 '''
 import torch
+import torch.nn as nn
+
+from datasets import LocalDataset
+from torch.utils.data import Dataset,DataLoader,random_split
+from torchvision import transforms
 
 
-class client(object):
+class Client(object):
     '''
     class for client object having its own private data and resources to train a model.
     
@@ -18,18 +23,103 @@ class client(object):
         model: the model will be trained in fedarated learning
     '''
 
-    def __init__(self,client_id,data_dir,device,model,malicous=False) -> None:
+    def __init__(self,client_id,data_dir,device,model,optim='sgd',malicous=False) -> None:
         self.id = client_id
         self.data_dir = data_dir
         self.device = device
         self.malicous = malicous
         self.model = model
-    
+        self.optim = optim
+
+
     def get_globalmodel(self,globalmodel_ckpt_file):
         globalmodel_ckpt = torch.load(globalmodel_ckpt_file)
         self.model.load_state_dict(globalmodel_ckpt)
     
     def save_model(self,localmodel_ckpt_path):
         torch.save(self.model.state_dict(),localmodel_ckpt_path)
+
+    def setup(self,transform=None,batchsize=128,local_epoch=1,lr=1e-3,train_factor=0.9,**client_config):
+        # create dataloader
+        if transform == None:
+            transform = transforms.Compose([transforms.ToTensor(),])
+        self.data = LocalDataset(data_dir=self.data_dir,transform=transform)
+        self.dataloader = DataLoader(self.data,batch_size=batchsize,shuffle=True)
+        # train_size = int(train_factor*len(self.data))
+        # test_size = len(self.data) - train_size
+        # self.train_dataset, self.test_dataset = random_split(self.data, [train_size, test_size])
+        # self.trainloader = DataLoader(self.train_dataset,batch_size=client_config["batch_size"],shuffle=True)
+        # self.testloader = DataLoader(self.test_dataset,batch_size=client_config["batch_size"],shuffle=True)
+
+        self.criterion = nn.CrossEntropyLoss()
+        if self.optim == 'sgd':
+            self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+        elif self.optim == 'adam':
+            self.optimizer =torch.optim.Adam(self.model.parameters(), lr=lr)
+        else:
+            raise KeyError('optim wrong')   
+        self.local_epoch = local_epoch
+
+    def client_update(self):
+        '''
+        update local model using local data
+        '''    
+        self.model.train()
+        self.model.to(self.device)
+
+        for _ in range(self.local_epoch):
+            for data,labels in self.dataloader:
+                data, labels = data.float().to(self.device), labels.long().to(self.device)
+  
+                self.optimizer.zero_grad()
+                outputs = self.model(data)
+                loss = self.criterion(outputs, labels)
+
+                loss.backward()
+                self.optimizer.step() 
     
-    
+    def client_evaluate(self):
+        """
+        Evaluate local model using local dataset 
+        """
+        self.model.eval()
+        self.model.to(self.device)
+
+        test_loss, correct = 0, 0
+        with torch.no_grad():
+            for data, labels in self.dataloader:
+                data, labels = data.float().to(self.device), labels.long().to(self.device)
+                outputs = self.model(data)
+                test_loss += self.criterion(outputs, labels).item()
+                
+                predicted = outputs.argmax(dim=1, keepdim=True)
+                correct += predicted.eq(labels.view_as(predicted)).sum().item()
+
+
+        test_loss = test_loss / len(self.dataloader)
+        test_accuracy = correct / len(self.data)
+
+        return test_loss, test_accuracy
+
+if __name__ == '__main__':
+    from torchvision import models
+    my_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.Grayscale(3),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,0.1307,0.1307), (0.3081,0.3081,0.3081)),
+    ])
+    model = models.resnet18()
+    in_features = model.fc.in_features
+    model.fc = nn.Linear(in_features, 10)
+    client0 = Client(
+                    client_id=0,
+                    data_dir='data/mnist_by_class',
+                    device='cuda',
+                    model = model,
+                    )
+    client0.setup(transform=my_transform,batchsize=1024)
+    for _ in range(10):
+        client0.client_update()
+        test_loss,test_acc = client0.client_evaluate()
+        print(test_acc)
--- src/datasets.py
+++ src/datasets.py
@@ -7,7 +7,7 @@ We want to randomly split the dataset into ten equal parts using MNIST as an exa
 After that, we will let each node have just one class for data to create a non-IID environment.
 Finally, we may be split by the hardness of data.
 '''
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset,DataLoader,random_split
 from torchvision import transforms
 from PIL import Image
 
@@ -15,6 +15,7 @@ import os
 import shutil
 from torchvision.datasets import MNIST
 from torchvision.transforms.functional import to_pil_image
+
 
 class LocalDataset(Dataset):
     def __init__(self,data_dir,transform=None):
@@ -39,13 +40,13 @@ class LocalDataset(Dataset):
             image = self.transform(image)
         return image, label
 
-
-
 def split_mnist_by_class(mnist_dir, output_dir):
     # 加载 MNIST 数据集
     mnist = MNIST(mnist_dir, download=True)
 
     # 遍历数据集中的每个图像和标签
+    if os.path.exists(output_dir):
+        return 
     for image_tensor, label in zip(mnist.data, mnist.targets):
         # 将图像张量转换为 PIL.Image 对象
         image = to_pil_image(image_tensor)
@@ -61,3 +62,18 @@ def split_mnist_by_class(mnist_dir, output_dir):
 if __name__ =='__main__':
     # 示例用法
     split_mnist_by_class('data/mnist', 'data/mnist_by_class')
+    trans = transforms.Compose([transforms.ToTensor(),
+			 transforms.Resize(256),
+			 transforms.Normalize((0.5),(0.5))
+             ])
+    data = LocalDataset(data_dir = 'data/mnist_by_class',transform=trans)
+    train_size = int(0.9 * len(data))
+    test_size = len(data) - train_size
+    train_dataset, test_dataset = random_split(data, [train_size, test_size])
+    
+    # dataloader = DataLoader(data,batch_size=128,shuffle=True)
+    # for i_batch,batch_data in enumerate(dataloader):
+    #     print(i_batch)#打印batch编号
+    #     # print(batch_data)
+    #     print(batch_data[0].size())#打印该batch里面图片的大小
+    #     print(batch_data[1])#打印该batch里面图片的标签
